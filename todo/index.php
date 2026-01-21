4d690f09d55cf57268bd85c61af234eb1c12b9b8
Emre Yiğit Kelleci <yisemu1@gmail.com>
2026-01-21 11:43:05 +0300

    todo_7 css güncellemeleri

--- todo/index.php
+++ todo/index.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <title>Mini To-Do</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
   <div class="box">
